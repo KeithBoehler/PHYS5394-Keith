ad5031fc89d5a224004bd018f6e3833ebfdd9d5c
KeithBoehler <germanbohler@gmail.com>
2021-02-10 17:12:37 -0600

    Changed the function to resembe a standard textbook sine wave. Also added header instructions

--- mySine.m
+++ mySine.m
@@ -1,12 +1,14 @@
 % Generic Sin, Keith Boehler
 % Intent: A function to call and generate sine funcs
-% Precond: Amplidue, phase, and other info will be in mem
+% Precond: Amplidue, phase (between 0-2pi), sampleing freq to generate sample vector,
+%         f0 for the ordinary freq (Hz)
 % Post Condition: Sine func generated 
 
 
-function mySineW = mySine(dataX, snr, coef)
-    phaseVec = coef * dataX; % Calculation of the phase vect
-    sigVec = sin(2*pi*phaseVec); % Calc of sig vector
-    sigVec = snr * (sigVec / norm(sigVec)); % Normalize
-    mySineW = sigVec; % set to return
+function mySineW = mySine(sampleFreq, f0, Amp, phase)
+    % Generate sample vect. Starts 0 and ends at 1
+    t = 0 : 1 / sampleFreq : 1; 
+    angularFreq = 2 * pi * f0; % aka omega
+    y = Amp * sin(angularFreq * t + phase);
+    mySineW = y;
 end

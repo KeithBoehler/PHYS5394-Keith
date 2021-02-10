89d4842370ba733eaa170b02e2c93de966dee884
KeithBoehler <germanbohler@gmail.com>
2021-02-10 17:16:50 -0600

    added a parameter for duration

--- mySine.m
+++ mySine.m
@@ -1,13 +1,14 @@
 % Generic Sin, Keith Boehler
 % Intent: A function to call and generate sine funcs
-% Precond: Amplidue, phase (between 0-2pi), sampleing freq to generate sample vector,
-%         f0 for the ordinary freq (Hz)
+% Precond: Amplidue, phase (between 0-2pi), sampleing freq to 
+%          generate sample vector, f0 for the ordinary freq (Hz)
+%          Duration in seconds. 
 % Post Condition: Sine func generated 
 
 
-function mySineW = mySine(sampleFreq, f0, Amp, phase)
-    % Generate sample vect. Starts 0 and ends at 1
-    t = 0 : 1 / sampleFreq : 1; 
+function mySineW = mySine(sampleFreq, f0, Amp, phase, T)
+    % Generate sample vect. Starts 0 and ends at T
+    t = 0 : 1 / sampleFreq : T; 
     angularFreq = 2 * pi * f0; % aka omega
     y = Amp * sin(angularFreq * t + phase);
     mySineW = y;

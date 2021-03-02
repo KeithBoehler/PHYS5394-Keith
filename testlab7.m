5a5aa004918877489176dd2a1887838102d3addb
KeithBoehler <germanbohler@gmail.com>
2021-03-02 00:31:43 -0600

    adding final comments

--- testlab7.m
+++ testlab7.m
@@ -24,6 +24,6 @@ figure;
 subplot(2,1,1), plot(uniformPDF), title('Uniform PDF'), xlabel('Trial'), ylabel('Value');
 subplot(2,1,2), histogram(uniformPDF, 'Normalization', 'pdf'), title('Histogram'), xlabel('Value'), ylabel('Probability');
 figure;
-subplot(2,1,1), plot(gaussPDF), title('Normal PDF'), xlabel('Trial'), ylabel('Value');
+subplot(2,1,1), plot(gaussPDF), title('Gauss PDF'), xlabel('Trial'), ylabel('Value');
 subplot(2,1,2), histogram(gaussPDF, 'Normalization', 'pdf'), title('Histogram'), xlabel('Value'), ylabel('Probability');
 

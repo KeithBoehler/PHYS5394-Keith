c6569389b569fa2a06c7c3b34a4e8fb8b6ee6da9
KeithBoehler <germanbohler@gmail.com>
2021-03-06 17:34:03 -0600

    data can now be loaded

---
+++ lab8test.m
@@ -0,0 +1,9 @@
+% Lab 8.1 Test script 
+
+% Load data into memory
+path = "/home/keith/Documents/Masters/Classes/Stat Methods/DATASCIENCE_COURSE/NOISE/testData.txt";
+data1 = load(path);
+% Seperate the data into columns 
+col1 = data1(:,1)
+col2 = data1(:,2)
+

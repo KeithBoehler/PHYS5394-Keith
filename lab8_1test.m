e12528c63452c3cf4b0a9068e3826d99c9693e17
KeithBoehler <germanbohler@gmail.com>
2021-03-13 14:22:52 -0600

    Name change of lab 8 to better relect the assigment parts, data is now included and searched with in the working dir

--- lab8_1test.m
+++ lab8_1test.m
@@ -1,7 +1,7 @@
 % Lab 8.1 Test script 
 
 % Load data into memory
-path = "/home/keith/Documents/Masters/Classes/Stat Methods/DATASCIENCE_COURSE/NOISE/testData.txt";
+path = "testData.txt";
 data1 = load(path);
 % Seperate the data into columns 
 col1 = data1(:,1);
